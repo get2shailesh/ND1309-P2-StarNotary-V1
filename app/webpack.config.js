34946e8aa4adbdf8e032cae74ae274491218a686
Shailesh Deshpande <get2shailesh@gmail.com>
2023-01-22 11:00:18 -0500

    Add files via upload

---
+++ app/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    hashFunction: "sha256",  // Lined added by SD based on google search
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+  ],
+  devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
+};
